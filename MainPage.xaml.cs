7047a0f9961ae36b0253d09903dc3bdc26f901aa
k1921 <k1921@OHJE-330-WS-31>
2017-03-30 10:01:20 +0300

    capman star

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -22,9 +22,42 @@ namespace Capman
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        //player
+        private Player player;
+
+        // private buttons pressed
+
+        // game loop timer
+        private DispatcherTimer timer;
+
+        
+        
+
         public MainPage()
         {
             this.InitializeComponent();
+            // add player
+            player = new Player
+            {
+                LocationX = MyCanvas.Width / 2,
+                LocationY = MyCanvas.Height / 2
+
+            };
+            MyCanvas.Children.Add(player);
+
+            
+
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60);
+            timer.Tick += Timer_Tick;
+            timer.Start(); //startgame
+
+
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+               
         }
     }
 }
